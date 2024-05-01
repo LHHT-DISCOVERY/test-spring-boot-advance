063ead6d1885b4663283458188ea59f10160261a
LHHT-DISCOVERY <lytri102@gmail.com>
2024-05-01 12:45:16 +0700

    ok Signed-off-by: LHHT-DISCOVERY <lytri102@gmail.com>

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -67,8 +67,7 @@ public class DemoApplication {
 //        bookService.saveOrUpdate(book);
 //    }//..,,.op2,op3,tro,tri2,test2-commit-first
 //(())
-
-//    tri_1,12,11
+//    tri_1,12, 1212312 ,tri_1,12,11
 //    }
 
 }
