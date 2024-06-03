75131aa278525bef0babc5b0d3b1de545516fe72
LHHT-DISCOVERY <lytri102@gmail.com>
2024-06-03 12:29:45 +0700

    validation custom annotation advance and extract information from annotation

--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.example.demo.exception;
 
 import com.example.demo.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
 import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final String MIN_ATTRIBUTE = "min";
+
     //catch error have unexpected cause by system not process yet
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingException(RuntimeException exception) {
@@ -33,20 +40,39 @@ public class GlobalExceptionHandler {
     //catch error cause by annotation @size ,...
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<ApiResponse> handlingMethodArgumentNotValid(MethodArgumentNotValidException exception) {
-        ApiResponse apiResponse = new ApiResponse();
         String enumKey = Objects.requireNonNull(exception.getFieldError()).getDefaultMessage();
-        ErrorCode errorCode = ErrorCode.valueOf(enumKey);
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map attributes = null;
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+
+//            get attributes in exception
+            var constraintViolation =
+                    exception.getBindingResult().getAllErrors().get(0).unwrap(ConstraintViolation.class);
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
+
+        } catch (IllegalArgumentException e) {
+
+        }
+        ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        // if have attribute -> map ; else -> get message original
+        apiResponse.setMessage(Objects.nonNull(attributes) ? mapAttribute(errorCode.getMessage(), attributes) : errorCode.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
-//    catch error cause while token valid but this token not permit implement task because this token not author
+    //    catch error cause while token valid but this token not permit implement task because this token not author
     @ExceptionHandler(value = AccessDeniedException.class)
     ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
         ErrorCode errorCode = ErrorCode.UNAUTHORiZED;
         return ResponseEntity.status(errorCode.getHttpStatusCode()).body(
                 ApiResponse.builder().code(errorCode.getCode()).message(errorCode.getMessage()).build()
         );
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE)); // get min = 18
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
---
+++ demo/src/main/java/com/example/demo/validator/DobConstraint.java
@@ -0,0 +1,25 @@
+package com.example.demo.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+//step1 validator
+//where this annotation(@Target) be apply? FILED apply to properties in object class
+@Target({ElementType.FIELD})
+//when this annotation(@Target) be processed?
+@Retention(RetentionPolicy.RUNTIME)
+//class response process for this validation
+@Constraint(
+        validatedBy = {DobValidator.class}
+)
+public @interface DobConstraint {
+    String message() default "Invalid date of birth";
+
+    int min();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
